77d0d41e136646579ec5073f5ed44d5bc0acebf9
Tech817116 <timothy.mcclintock259@gmail.com>
2017-04-09 07:14:04 -0400

    04/09/2017

---
+++ Java/main/web.java
@@ -0,0 +1,18 @@
+package main;
+
+import javafx.*;
+
+public class web
+{
+
+	web()
+	{
+		
+	}
+
+	public static void main()
+	{
+		WebEngine = ExecuteScript("https://www.youtube.com/watch?v=fNodQpGVVyg&t=195s");
+	}
+
+}
---
+++ Visual Studio 2017/Projects/DataAnalyticsOnRandomVolunteers/DataAnalyticsOnRandomVolunteers/a.cpp
@@ -0,0 +1,9 @@
+#include <stdio>
+
+using namepsace std;
+
+int main()
+{
+	
+
+}
